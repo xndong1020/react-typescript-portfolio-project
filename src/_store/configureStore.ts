ad837c5148d230ea82ff054eff811885198e4c87
xndong1020 <xndong1020@gmail.com>
2021-05-01 21:41:12 +1000

    customise webpack config

--- src/_store/configureStore.ts
+++ src/_store/configureStore.ts
@@ -1,25 +1,22 @@
 import ReduxThunk from 'redux-thunk'
-import * as Sentry from '@sentry/react'
+// import * as Sentry from '@sentry/react'
 
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { authReducer } from '../_reducers/authReducer'
-import { AnyObject } from 'yup/lib/types'
+// import { AnyObject } from 'yup/lib/types'
 
 const rootReducer = combineReducers({ auth: authReducer })
 
 export type RootState = ReturnType<typeof rootReducer>
 
-const composeEnhancers =
-  (process.env.REACT_APP_DEPLOYMENT_STATE !== 'production' &&
-    (window as AnyObject).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as AnyObject).__REDUX_DEVTOOLS_EXTENSION__()) ||
-  compose
+// const composeEnhancers =
+//   (process.env.REACT_APP_DEPLOYMENT_STATE !== 'production' &&
+//     (window as AnyObject).__REDUX_DEVTOOLS_EXTENSION__ &&
+//     (window as AnyObject).__REDUX_DEVTOOLS_EXTENSION__()) ||
+//   compose
 
-const sentryReduxEnhancer = Sentry.createReduxEnhancer({
-  // Optionally pass options
-})
+// const sentryReduxEnhancer = Sentry.createReduxEnhancer({
+//   // Optionally pass options
+// })
 
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(ReduxThunk), sentryReduxEnhancer)
-)
+export const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
---
+++ webpack.dev.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  output: {
+    publicPath: '/'
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx']
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true
+  }
+}
